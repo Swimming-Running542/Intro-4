51fc4b7858f5cd0c068ed0b65c3cb2fefb1dd298
SwimmingRunning <bellaparkkj@gmail.com>
2020-07-13 16:55:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+num = int(input("input a number: "))
+if num > 10:
+  print("Your number is greater then 10")
+
+elif num < 10:
+  print("Your number is less then 10")
+elif num == 10:
+  print("Your number is 10")
+  
